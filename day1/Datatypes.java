df483f414691e25cfc22f2f793631ba166f2bfb1
123testleaf <127011177+123testleaf@users.noreply.github.com>
2023-03-05 13:28:34 +0530

    Add files via upload
    day1

---
+++ day1/Datatypes.java
@@ -0,0 +1,27 @@
+package week.day1;
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		   int carbraks = 4;
+		   boolean  train = true;
+		   
+		    String carcoller = "red";
+		    
+		    String carcoller1 = "zzzzzzzzzdd";
+		    
+		   float       mailige = 11.4f;
+		    
+		   
+		   System.out.println(carbraks);
+		   
+		   System.out.println(carcoller);
+		   System.out.println(carcoller1);
+		   System.out.println(mailige +carcoller1 +train);
+		   
+	}
+	
+
+}
