a767c6a908db2df97edf081fe6122c43ac40137e
123testleaf <127011177+123testleaf@users.noreply.github.com>
2023-03-12 10:29:29 +0530

    Add files via upload

---
+++ day1/Assignment1.java
@@ -0,0 +1,62 @@
+package week2.day1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class Assignment1 {
+
+	public static void main(String[] args) {
+		
+		
+		ChromeDriver driver=new ChromeDriver();
+		
+		driver.get("http://leaftaps.com/opentaps/control/login");
+		
+		driver.manage().window().maximize();
+		
+		driver.findElement(By.id("username")).sendKeys("DemoCSR");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		
+		driver.findElement(By.partialLinkText("CRM/SFA")).click();
+		
+	driver.findElement(By.linkText("Leads")).click();
+	driver.findElement(By.partialLinkText("Create Lead")).click();
+	driver.findElement(By.id("createLeadForm_companyName")).sendKeys("TCS");
+	driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Ganesh");
+	driver.findElement(By.id("createLeadForm_lastName")).sendKeys("yeddula");
+	driver.findElement(By.id("createLeadForm_firstNameLocal")).sendKeys("Ganesh reddy yeddula");
+	driver.findElement(By.id("createLeadForm_departmentName")).sendKeys("testing team");
+	driver.findElement(By.id("createLeadForm_description")).sendKeys("automation testing");
+	driver.findElement(By.id("createLeadForm_primaryEmail")).sendKeys("yeddula.195@gmail.com");
+	WebElement state =  driver.findElement(By.id("createLeadForm_generalStateProvinceGeoId"));
+	
+    Select sta = new Select(state);
+    sta.selectByVisibleText("New York");
+    
+    driver.findElement(By.id("createLeadForm_generalPostalCode")).sendKeys("514471");
+   driver.findElement(By.name("submitButton")).click();
+   
+   String title = driver.getTitle();
+   
+   System.out.println(title);
+   
+   driver.close();
+   
+   
+   
+   
+   
+	
+	
+		
+		
+		
+		
+		
+
+	}
+
+}
